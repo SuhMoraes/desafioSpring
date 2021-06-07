94e0d207b7626fb45afb54794cb1f4d9bfcb400e
Suh Moraes <suh.alvesmoraes@gmai.com>
2021-06-07 11:44:50 -0300

    Created Project

---
+++ desafioorangetalents/src/main/java/br/com/suhmoraes/desafioorangetalents/DesafioorangetalentsApplication.java
@@ -0,0 +1,13 @@
+package br.com.suhmoraes.desafioorangetalents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioorangetalentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioorangetalentsApplication.class, args);
+	}
+
+}
---
+++ desafioorangetalents/src/test/java/br/com/suhmoraes/desafioorangetalents/DesafioorangetalentsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.suhmoraes.desafioorangetalents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioorangetalentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
