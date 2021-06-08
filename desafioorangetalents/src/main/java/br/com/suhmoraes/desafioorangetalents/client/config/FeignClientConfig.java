90c4ec0105699dda9df1dcbe21a19980a78abbb9
Suh Moraes <suh.alvesmoraes@gmai.com>
2021-06-07 22:57:50 -0300

    Users retrieval and rotation day response injection

---
+++ desafioorangetalents/src/main/java/br/com/suhmoraes/desafioorangetalents/client/config/FeignClientConfig.java
@@ -0,0 +1,5 @@
+package br.com.suhmoraes.desafioorangetalents.client.config;
+
+public class FeignClientConfig {
+
+}
---
+++ desafioorangetalents/src/main/java/br/com/suhmoraes/desafioorangetalents/model/CarsResponse.java
@@ -0,0 +1,5 @@
+package br.com.suhmoraes.desafioorangetalents.model;
+
+public class CarsResponse {
+
+}
